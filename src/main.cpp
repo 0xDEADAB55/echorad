d71b53e5b21916d3353f7b6a96aef7af8ba3c8e5
0xDEADAB55 <aslan150495@gmail.com>
2026-06-09 01:50:28 +0300

    check

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 #include <Arduino.h>
 #include "variant.h"
 
-#define LED PIN_015 //Set a definiton on pin P0.15 called "LED".
+#define RED_LED PIN_015 //Set a definiton on pin P0.15 called "LED".
 
 void setup() {
-  pinMode(LED, OUTPUT); //Set the LED to output mode.
+  pinMode(RED_LED, OUTPUT); //Set the LED to output mode.
 }
 
 void loop() {
-  digitalWrite(LED, LOW); //Set the LED to low
+  digitalWrite(RED_LED, LOW); //Set the LED to low
   delay(1000); //wait 1s
-  digitalWrite(LED, HIGH); //Set the LED to high
-  delay(1000); //wait 1s
+  digitalWrite(RED_LED, HIGH); //Set the LED to high
+  delay(3000); //wait 3s
 }
